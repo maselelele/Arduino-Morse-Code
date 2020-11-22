3dcb6e19d689e59c7cd82c4ba0f455fde21f2464
maselelele <contact@masel.dev>
2020-11-22 04:36:33 +0100

    Add morse funcionality

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
 const five = require('johnny-five');
 const board = new five.Board();
 
+const codes = {
+    A: '·−',
+    B: '−···',
+    C: '−·−·',
+    D: '−··',
+    E: '·',
+    F: '··−·',
+    G: '−−·',
+    H: '····',
+    I: '··',
+    J: '·−−−',
+    K: '−·−',
+    L: '·−··',
+    M: '−−',
+    N: '−·',
+    O: '−−−',
+    P: '·−−·',
+    Q: '−−·−',
+    R: '·−·',
+    S: '···',
+    T: '−',
+    U: '··−',
+    V: '···−',
+    W: '·−−',
+    X: '−··−',
+    Y: '−·−−',
+    Z: '−−··'
+};
+
+let letter = [];
+let word = [];
+
 board.on('ready', () => {
     let button = new five.Button(5);
+    let count = Date.now();
 
     button.on('press', () => {
-        console.log('press');
-    });
-
-    button.on('release', () => {
-        console.log('release');
+        if (checkBrake(count)) {
+            console.log('long');
+            word.push(convertLetter(letter.join('')));
+            letter = [];
+            letter.push('·');
+        } else {
+            console.log('short');
+            letter.push('·');
+        }
     });
 
     button.on('hold', () => {
-        console.log('hold');
+        letter.pop();
+        letter.push('−');
     });
 
+    button.on('release', () => {
+        count = Date.now();
+        console.log(`word: ${word.join('')}`);
+        console.log(`letter ${letter.join('')}`);
+    });
 });
+
+function checkBrake(time) {
+    return (time !== 0 && Date.now() - time < 1000) ? false : true;
+}
+
+function convertLetter(letter) {
+    for (const [key, value] of Object.entries(codes)) {
+        if (letter === value) return key;
+    }
+}
